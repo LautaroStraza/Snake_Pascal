bde9090bd381779cadf314f2a38edb98d5186c0b
lautaro <lau_mb94@hotmail.com>
2018-11-25 19:59:12 -0300

    version 1.1, Se agrego comida

--- main.pas
+++ main.pas
@@ -15,12 +15,38 @@ var
 	key: char;
 	arrow: boolean;
 	matriz: array of array of char;
+
+	(* El siguiente vector guardara la siguiente información:
+		cuerpo[Nodo_serpiente, tipo_de_Nodo, pos_X, pos_Y] *)
+	cuerpo: array of array [1..3] of integer;
+	(* Defino un índice para leer dentro del cuerpo de la serpiente *)
+	nodos: integer;
+	(* De este modo puedo referirme 
+	a la cabeza de la serpiente como:
+			cuerpo[1, 1]= -1; 
+	cuya posición sera:
+			cuerpo[1, 2]= pos_x; 
+			cuerpo[1, 3]= pos_y; 
+
+	a cuaquier nodo del cuerpo de la serpiente como:
+			cuerpo[nodos, 1]= 0;
+	cuya posición sera:
+			cuerpo[nodos, 2]= pos_x; 
+			cuerpo[nodos, 3]= pos_y; 
+	a la cola de la serpiente como:
+			cuerpo[nodos, 1]= 1;
+	cuya posición sera:
+			cuerpo[nodos, 2]= pos_x; 
+			cuerpo[nodos, 3]= pos_y; *)
+
 	filas, columnas: integer; (* Para definir el tamaño de la matriz *)
 	f, c: integer; (* Indices para imprimir la matriz *)
-	cabeza_x, cabeza_y: integer;(* Indices para moverme dentro de la matriz *)
-	cola_x, cola_y: integer;(* Indices para moverme dentro de la matriz *)
+	pos_x, pos_y: integer;(* Indices para moverme dentro de la matriz *)
+	indice: integer; (* Para buscar dentro del cuerpo cual es la cola o la cabeza *)
+	aux: integer; (* Para buscar dentro del cuerpo cual es la cola o la cabeza *)
 	continuar: boolean;
 	ultimo_movimiento: integer;
+	puntos: integer;
 
 (* Inicio de procedimientos y funciones particulares del programa *)
 
@@ -38,16 +64,29 @@ begin (* Inicio programa principal *)
         columnas:= screenwidth - 3; (*Defino la cantidad de columnas según la ventana *)
         SetLength(matriz, filas + 1, columnas); (* Defino el tamaño de la matriz *)
 	continuar:= true; (* Condición para seguir jugando *)
-	ultimo_movimiento:= 72;
+	ultimo_movimiento:= 72; (* Comienza el juego moviendose hacia arriba *)
+	puntos:= 0;
+
+	nodos:= filas * columnas; (* Defino la cantidad de nodos máximos de la serpiente, que dependerá del tamaño de la matriz *)
+	(* Inicializo el cuerpo de Snake *)
+        SetLength(cuerpo, nodos); (* Defino el tamaño de la matriz *)
+	for f:=1 to nodos do
+		begin
+		cuerpo[f, 1]:= 0;
+		cuerpo[f, 2]:= 0;
+		cuerpo[f, 3]:= 0;
+		end;
+
 
         (* Inicializo la matriz *)
         for f:=1 to filas do
                 begin
                 for c:=1 to columnas do
                         begin
-                        matriz[f,c]:= '.';
+                        matriz[f,c]:= ' ';
                         end;
                 end;
+
 
 	(* Edito primera fila *)
         for c:=1 to columnas do
@@ -61,17 +100,25 @@ begin (* Inicio programa principal *)
         WriteLn('Cantidad de columnas:',columnas);
         WriteLn();
 
-	(* Elijo la posición inicial de la marca *)
-	cabeza_x:= random(filas);
-	if (cabeza_x = 1) then
-		cabeza_x:= cabeza_x + 1
-	else if (cabeza_x = filas) then
-		cabeza_x:= cabeza_x - 2;
-	cola_x:= cabeza_x - 1;
-	cabeza_y:= random(columnas);
-	cola_y:= cabeza_y;
-	matriz[cabeza_x, cabeza_y]:= 'X';
-	matriz[cola_x, cola_y]:= 'X';
+	(* Elijo la posición inicial de la cabeza *)
+	pos_x:= random(filas);
+	if (pos_x = 1) then
+		pos_x:= pos_x + 1
+	else if (pos_x = filas) then
+		pos_x:= pos_x - 2;
+	
+	pos_y:= random(columnas);
+	
+	cuerpo[1,1]:= -1; (* Nodo cabeza de serpiente *)
+	cuerpo[1,2]:= pos_x;
+	cuerpo[1,3]:= pos_y;
+	
+	cuerpo[2,1]:= 1; (* Nodo cabeza de serpiente *)
+	cuerpo[2,2]:= pos_x;
+	cuerpo[2,3]:= pos_y - 1 ;
+
+	matriz[cuerpo[1,2], cuerpo[1,3]]:= 'X';
+	matriz[cuerpo[2,2], cuerpo[2,3]]:= 'X';
 	
 	(* Selecciono opción *)
 	repeat
@@ -84,6 +131,14 @@ begin (* Inicio programa principal *)
 				end;
 			WriteLn();
 			end;
+
+		puntos:= puntos + 1;
+
+		(* Agrego comida aleatoriamente *)
+		pos_x:= random(columnas-1)+1;
+		pos_y:= random(filas-1)+1;
+		if (matriz[pos_y, pos_x] = ' ') and (puntos mod 50 = 0) then
+			matriz[pos_y, pos_x]:= 'o';
 		
 		Delay(100);					(* Tiempo entre "FPS's" *)
 
@@ -128,61 +183,193 @@ begin (* Inicio programa principal *)
 		
 		case ultimo_movimiento of			(* Las opciones a seguir para cada flecjha *)
 			72: 
-				begin
-				(*WriteLn('arriba');*)
+				begin (* Arriba *)
 				ultimo_movimiento:= 72;
-				matriz[cola_x, cola_y]:= '.'; (* Limpio la posición vieja *)
-				cola_x:= cabeza_x;
-				cola_y:= cabeza_y;
-				if (cabeza_x > 1) then
-					cabeza_x:= cabeza_x - 1 ;
-				if (cabeza_x = 1) then
-					continuar:= false;
-				matriz[cabeza_x, cabeza_y]:= 'X';	(* Marco la nueva posición *)
+				(* Busco el nodo cola para borrarlo y asignar la cola en la posición del nodo anterior *)
+				for indice:= nodos downto 2 do 
+					begin
+					if (cuerpo[indice,1] = 1) then (* Nodo de la cola *)
+						begin
+						if (matriz[cuerpo[1,2]-1, cuerpo[1,3]] = 'o') then (* Dejo que crezca si el punto sobre la cabeza era comida *)
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								cuerpo[indice,1]:= 0; (* La cola se vuelve un nodo común *)
+								cuerpo[indice+1,1]:= 1; (* Se agrega un nodo y se convierte en la nueva cola *)
+								for aux:=indice+1 downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end
+							else
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								for aux:=indice downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end;
+						end;
+					if (matriz[cuerpo[1,2]-1, cuerpo[1,3]] = 'X') then (* Para perder la partida si se pisa a si mismo *)
+							continuar:= false;
+					end;
+				(* Busco el nodo cabeza para asignarle una nueva posición *)
+				for indice:= 1 to nodos do 
+					begin
+					if (cuerpo[indice,1] = -1) then (* Nodo de la cabeza *)
+						begin
+
+						if (cuerpo[indice,2] > 1) then
+							cuerpo[indice,2]:= cuerpo[indice,2] - 1 ;
+						if (cuerpo[indice,2] = 1) then
+							continuar:= false;
+						matriz[cuerpo[indice,2], cuerpo[indice,3]]:= 'X'; (* Marco la nueva posicón *)
+						end;
+					end;
 				end;
 			80: 
-				begin
-				(*WriteLn('abajo');*)
+				begin (* Abajo *)
 				ultimo_movimiento:= 80;
-				matriz[cola_x, cola_y]:= '.'; (* Limpio la posición vieja *)
-				cola_x:= cabeza_x;
-				cola_y:= cabeza_y;
-				if (cabeza_x < filas) then
-					cabeza_x:= cabeza_x + 1 ;
-				if (cabeza_x = filas) then
-					continuar:= false;
-				matriz[cabeza_x, cabeza_y]:= 'X';	(* Marco la nueva posición *)
+				(* Busco el nodo cola para borrarlo y asignar la cola en la posición del nodo anterior *)
+				for indice:= nodos downto 2 do 
+					begin
+					if (cuerpo[indice,1] = 1) then (* Nodo de la cola *)
+						begin
+						if (matriz[cuerpo[1,2]+1, cuerpo[1,3]] = 'o') then (* Dejo que crezca si el punto debajo de la cabeza era comida *)
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								cuerpo[indice,1]:= 0; (* La cola se vuelve un nodo común *)
+								cuerpo[indice+1,1]:= 1; (* Se agrega un nodo y se convierte en la nueva cola *)
+								for aux:=indice+1 downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end
+							else
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								for aux:=indice downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end;
+						end;
+					if (matriz[cuerpo[1,2]+1, cuerpo[1,3]] = 'X') then (* Para perder la partida si se pisa a si mismo *)
+							continuar:= false;
+					end;
+				(* Busco el nodo cabeza para asignarle una nueva posición *)
+				for indice:= 1 to nodos do 
+					begin
+					if (cuerpo[indice,1] = -1) then (* Nodo de la cabeza *)
+						begin
+
+						if (cuerpo[indice,2] < filas) then
+							cuerpo[indice,2]:= cuerpo[indice,2] + 1 ;
+						if (cuerpo[indice,2] = filas) then
+							continuar:= false;
+						matriz[cuerpo[indice,2], cuerpo[indice,3]]:= 'X'; (* Marco la nueva posicón *)
+						end;
+					end;
 				end;
-			75:
-				begin
-				(*WriteLn('izquierda');*)
+			75: 
+				begin (* Izquierda *)
 				ultimo_movimiento:= 75;
-				matriz[cola_x, cola_y]:= '.'; (* Limpio la posición vieja *)
-				cola_x:= cabeza_x;
-				cola_y:= cabeza_y;
-				if (cabeza_y > 0) then
-					cabeza_y:= cabeza_y - 1 ;
-				if (cabeza_y = 0) then
-					continuar:= false;
-				matriz[cabeza_x, cabeza_y]:= 'X';	(* Marco la nueva posición *)
+				(* Busco el nodo cola para borrarlo y asignar la cola en la posición del nodo anterior *)
+				for indice:= nodos downto 2 do 
+					begin
+					if (cuerpo[indice,1] = 1) then (* Nodo de la cola *)
+						begin
+						if (matriz[cuerpo[1,2], cuerpo[1,3]-1] = 'o') then (* Dejo que crezca si el punto a la izquierda de la cabeza era comida *)
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								cuerpo[indice,1]:= 0; (* La cola se vuelve un nodo común *)
+								cuerpo[indice+1,1]:= 1; (* Se agrega un nodo y se convierte en la nueva cola *)
+								for aux:=indice+1 downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end
+							else
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								for aux:=indice downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end;
+						end;
+					if (matriz[cuerpo[1,2], cuerpo[1,3]-1] = 'X') then (* Para perder la partida si se pisa a si mismo *)
+							continuar:= false;
+					end;
+				(* Busco el nodo cabeza para asignarle una nueva posición *)
+				for indice:= 1 to nodos do 
+					begin
+					if (cuerpo[indice,1] = -1) then (* Nodo de la cabeza *)
+						begin
+
+						if (cuerpo[indice,3] > 0) then
+							cuerpo[indice,3]:= cuerpo[indice,3] - 1 ;
+						if (cuerpo[indice,3] = 0) then
+							continuar:= false;
+						matriz[cuerpo[indice,2], cuerpo[indice,3]]:= 'X'; (* Marco la nueva posicón *)
+						end;
+					end;
 				end;
-			77: 
-				begin
-				(*WriteLn('derecha');*)
+			77:  
+				begin (* Derecha *)
 				ultimo_movimiento:= 77;
-				matriz[cola_x, cola_y]:= '.'; (* Limpio la posición vieja *)
-				cola_x:= cabeza_x;
-				cola_y:= cabeza_y;
-				if (cabeza_y < columnas + 1) then
-					cabeza_y:= cabeza_y + 1 ;
-				if (cabeza_y = columnas + 1) then
-					continuar:= false;
-				matriz[cabeza_x, cabeza_y]:= 'X';	(* Marco la nueva posición *)
+				(* Busco el nodo cola para borrarlo y asignar la cola en la posición del nodo anterior *)
+				for indice:= nodos downto 2 do 
+					begin
+					if (cuerpo[indice,1] = 1) then (* Nodo de la cola *)
+						begin
+						if (matriz[cuerpo[1,2], cuerpo[1,3]+1] = 'o') then (* Dejo que crezca si el punto a la derecha de la cabeza era comida *)
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								cuerpo[indice,1]:= 0; (* La cola se vuelve un nodo común *)
+								cuerpo[indice+1,1]:= 1; (* Se agrega un nodo y se convierte en la nueva cola *)
+								for aux:=indice+1 downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end
+							else
+								begin
+								matriz[cuerpo[indice,2], cuerpo[indice,3]]:= ' '; (* Limpio la posición vieja *)
+								for aux:=indice downto 2 do
+									begin
+									cuerpo[aux,2]:= cuerpo[aux-1,2];
+									cuerpo[aux,3]:= cuerpo[aux-1,3];
+									end;
+								end;
+						end;
+					if (matriz[cuerpo[1,2], cuerpo[1,3]+1] = 'X') then (* Para perder la partida si se pisa a si mismo *)
+							continuar:= false;
+					end;
+				(* Busco el nodo cabeza para asignarle una nueva posición *)
+				for indice:= 1 to nodos do 
+					begin
+					if (cuerpo[indice,1] = -1) then (* Nodo de la cabeza *)
+						begin
+
+						if (cuerpo[indice,3] < columnas + 1) then
+							cuerpo[indice,3]:= cuerpo[indice,3] + 1 ;
+						if (cuerpo[indice,3] = columnas +1) then
+							continuar:= false;
+						matriz[cuerpo[indice,2], cuerpo[indice,3]]:= 'X'; (* Marco la nueva posicón *)
+						end;
+					end;
 				end;
 			end;
 	until (continuar <> true); 					(* El valor de la tecla especial enter es 13, lo uso para salir *)
 	ClrScr();
-	WriteLn('Puntuación final: ');
+	WriteLn('Puntuación final: ', puntos);
 	WriteLn('Enter para salir...');
 	ReadLn();
 
